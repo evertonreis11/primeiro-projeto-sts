4d9b08d3f4642a2b5324859017368d1deb8a38de
Everton Reis <everton.reis19@gmail.com>
2019-05-04 12:07:25 -0300

    Arquivos iniciais do projeto

---
+++ src/main/java/com/curso/springboot/api/PrimeiroProjetoStsApplication.java
@@ -0,0 +1,13 @@
+package com.curso.springboot.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrimeiroProjetoStsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrimeiroProjetoStsApplication.class, args);
+	}
+
+}
